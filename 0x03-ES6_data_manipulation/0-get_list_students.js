73f281b3146f974b0ef93d17a47d451df3567f73
root <anas.baidouri@gmail.com>
2024-04-16 21:49:09 +0000

    0-get

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+//export getlist from 0-get_list_students.js
+export default function getListStudents()
+	{
+		return [
+			{
+				id: 1, firstName: 'Guillaume', location: 'San Francisco'
+			},
+			{
+				d: 2, firstName: 'James', location: 'Columbia'
+			},
+			{
+				id: 5, firstName: 'Serena', location: 'San Francisco'
+			}
+		];
+	}
